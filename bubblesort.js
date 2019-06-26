3842fb38d20a1822f5fd97cf5b51955da4ffbf4e
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-06-26 12:50:05 +0200

    complete visualization

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,28 @@
-let sort = (array) => {
+// let sort = (array) => {
+//     let swapped = true;
+//     do {
+//         swapped = false;
+//         for (let i = 0; i < Object.keys(array).length; i++) {
+//             if (array[i] > array[i + 1]) {
+//                 setTimeout(() => {
+//                     let temp = array[i]
+//                     array[i] = array[i + 1]
+//                     array[i + 1] = temp
+//                     swapped = true
+//                 }, 10)
+//             }
+//         }
+//     } while (swapped)
+//     displayColumns(array)
+//     return array
+// }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+let sort = async (array) => {
     let swapped = true;
     do {
         swapped = false;
@@ -8,11 +32,14 @@ let sort = (array) => {
                 array[i] = array[i + 1]
                 array[i + 1] = temp
                 swapped = true
+                displayColumns(array)
+                await sleep(1)
             }
         }
     } while (swapped)
     return array
 }
+
 
 let generateArray = (number) => {
     let array = {}
@@ -23,9 +50,8 @@ let generateArray = (number) => {
 }
 
 let displayColumns = (array) => {
-    [].forEach.call(document.querySelectorAll('.column'), function (e) {
-        e.parentNode.removeChild(e);
-    });
+    document.getElementsByClassName("columns")[0].innerHTML = ""
+
     for (let i = 0; i < Object.keys(array).length; i++) {
         let column = document.createElement("div")
         column.className = "column"
@@ -36,8 +62,8 @@ let displayColumns = (array) => {
     }
 }
 
-let array = generateArray(200)
+let array = generateArray(50)
 displayColumns(array)
 document.getElementById('sortBtn').addEventListener('click', () => {
-    displayColumns(sort(array))
+    sort(array)
 })
