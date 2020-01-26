bb176236f8afec2a14b9e239c7f9783018e8993b
Bartosz Kępka <bartoszkepka1999@gmail.com>
2020-01-26 19:21:11 +0100

    Add state, reset button and reset function

--- bubblesort.js
+++ bubblesort.js
@@ -1,69 +1,65 @@
-// let sort = (array) => {
-//     let swapped = true;
-//     do {
-//         swapped = false;
-//         for (let i = 0; i < Object.keys(array).length; i++) {
-//             if (array[i] > array[i + 1]) {
-//                 setTimeout(() => {
-//                     let temp = array[i]
-//                     array[i] = array[i + 1]
-//                     array[i + 1] = temp
-//                     swapped = true
-//                 }, 10)
-//             }
-//         }
-//     } while (swapped)
-//     displayColumns(array)
-//     return array
-// }
+let state = {
+    columnsArray: []
+};
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
-let sort = async (array) => {
+let sort = async array => {
     let swapped = true;
     do {
         swapped = false;
         for (let i = 0; i < Object.keys(array).length; i++) {
             if (array[i] > array[i + 1]) {
-                let temp = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = temp
-                swapped = true
-                displayColumns(array)
-                await sleep(1)
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+                displayColumns(array);
+                await sleep(1);
             }
         }
-    } while (swapped)
-    return array
-}
+    } while (swapped);
+    return array;
+};
 
-
-let generateArray = (number) => {
-    let array = {}
+let generateArray = number => {
+    let array = {};
     for (let i = 0; i < number; i++) {
-        array[i] = Math.floor(Math.random() * 29) + 1
+        array[i] = Math.floor(Math.random() * 40) + 1;
     }
-    return array
-}
+    return array;
+};
 
-let displayColumns = (array) => {
-    document.getElementsByClassName("columns")[0].innerHTML = ""
+let displayColumns = array => {
+    document.getElementsByClassName("columns")[0].innerHTML = "";
 
     for (let i = 0; i < Object.keys(array).length; i++) {
-        let column = document.createElement("div")
-        column.className = "column"
-        column.style.backgroundColor = "white"
-        column.style.height = `${array[i]}vh`
-        column.style.width = '5px'
-        document.getElementsByClassName("columns")[0].appendChild(column)
+        let column = document.createElement("div");
+        column.className = "column";
+        column.style.backgroundColor = "white";
+        column.style.height = `${array[i]}vh`;
+        column.style.width = "10px";
+        document.getElementsByClassName("columns")[0].appendChild(column);
     }
-}
+};
 
-let array = generateArray(50)
-displayColumns(array)
-document.getElementById('sortBtn').addEventListener('click', () => {
-    sort(array)
-})
+let reset = () => {
+    document.getElementsByClassName("columns")[0].innerHTML = "";
+    state.columnsArray = generateArray(50);
+    displayColumns(state.columnsArray);
+};
+
+let render = () => {
+    state.columnsArray = generateArray(50);
+    displayColumns(state.columnsArray);
+    document.getElementById("sortBtn").addEventListener("click", () => {
+        sort(state.columnsArray);
+    });
+    document.getElementById("resetBtn").addEventListener("click", () => {
+        reset();
+    });
+};
+
+render();
